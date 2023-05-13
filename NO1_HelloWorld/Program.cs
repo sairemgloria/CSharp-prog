16821dd1a1cbea92dde203800b753eacda80b874
sairemgloria <sairemgloria1999@gmail.com>
2023-05-13 16:17:48 +0800

    Add project files.

---
+++ NO1_HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NO1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
