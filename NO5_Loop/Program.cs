09835782d79ba04e2ec594114596cc279a8199af
sairemgloria <sairemgloria1999@gmail.com>
2023-05-13 20:50:36 +0800

    Added topic NO5_Loops.

---
+++ NO5_Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace NO5_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic 5 : Loops */
+            Console.WriteLine("Topic 5 : Loops"); // -> display title
+            Console.WriteLine("OUTPUT FOR WHILE LOOP: ");
+            WhileLoop();
+            Console.WriteLine("OUTPUT FOR DO WHILE LOOP: ");
+            DoWhileLoop();
+        }
+
+        /* While Loop Function */
+        static void WhileLoop()
+        {
+            int i = 1; // -> declared variable, count starts at 1 to 5
+            while (i <= 5) // -> check the condition
+            {
+                Console.WriteLine("Hello, world!", i); // -> output
+                i++; // -> iteration or increment
+            }
+        }
+
+        /* Do While Loop Function */
+        static void DoWhileLoop()
+        {
+            int j = 1; // -> declared variable, count starts at 1 to 5
+            do
+            {
+                Console.WriteLine("Henlo, mundo!", j); // -> output
+                j++; // -> iteration or increment
+            } while (j <= 5); // -> check the condition
+        }
+    }
+}
