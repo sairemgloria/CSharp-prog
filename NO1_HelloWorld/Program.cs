b07050a0ff9a1783416a60f9cecdab3ce0d563f8
sairemgloria <sairemgloria1999@gmail.com>
2023-05-13 16:45:03 +0800

    This is my first sample program using C#.

--- NO1_HelloWorld/Program.cs
+++ NO1_HelloWorld/Program.cs
@@ -6,6 +6,7 @@ namespace NO1_HelloWorld
     {
         static void Main(string[] args)
         {
+            /* This is my first sample program using C# programming */
             Console.WriteLine("Hello, World!");
         }
     }
