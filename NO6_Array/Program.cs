22a29507ead85ece0237d5ee18a362cc3438a397
sairemgloria <sairemgloria1999@gmail.com>
2023-05-13 21:55:46 +0800

    Added topic NO6_Array.

---
+++ NO6_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NO6_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic 6 : Array */
+            Console.WriteLine("Topic 6 : Array"); // -> display title
+            Arrays(); // -> display the output of function
+        }
+
+        static void Arrays()
+        {
+            string[] cars = {"Toyota","BMW","Ferrari"};
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine("Car Brand : " + cars[i]);
+            }
+        }
+    }
+}
