649388d2df5ec83dcbbc0f41ae15cea0d554fe48
sairemgloria <sairemgloria1999@gmail.com>
2023-05-14 18:51:16 +0800

    Added foreach on topic NO6_Array

--- NO6_Array/Program.cs
+++ NO6_Array/Program.cs
@@ -7,14 +7,27 @@
             /* Topic 6 : Array */
             Console.WriteLine("Topic 6 : Array"); // -> display title
             Arrays(); // -> display the output of function
+            Console.WriteLine();
+            ForeachTuts(); // -> display the output of foreach function
         }
 
+        /* Array */
         static void Arrays()
         {
             string[] cars = {"Toyota","BMW","Ferrari"};
             for (int i = 0; i < cars.Length; i++)
             {
                 Console.WriteLine("Car Brand : " + cars[i]);
+            }
+        }
+
+        /* Foreach */
+        static void ForeachTuts()
+        {
+            string[] Fruits = { "Apple", "Banana", "Mango" };
+            foreach (string f in Fruits)
+            {
+                Console.WriteLine(f);
             }
         }
     }
