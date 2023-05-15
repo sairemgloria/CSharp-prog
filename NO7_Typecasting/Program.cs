5a7ca30397799a6e5c2ea8cde450bf9d485b6971
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-15 09:21:20 +0800

    Added topic NO7_Typecasting.

---
+++ NO7_Typecasting/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace NO7_Typecasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic 7 : Typecasting */
+            Console.WriteLine("Topic 7 : Typecasting"); // -> display title
+            SumProcess();
+        }
+
+        static void SumProcess()
+        {
+            Console.WriteLine("Enter first number : ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number : ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of two number is : {0}", firstNumber + secondNumber);
+        }
+    }
+}
