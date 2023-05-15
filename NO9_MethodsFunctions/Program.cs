162636218b860d449579a54a831d3d936e78c465
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-15 13:00:50 +0800

    Added topic NO9_MethodsFunctions.

---
+++ NO9_MethodsFunctions/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NO9_MethodsFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic 9 : Methods */
+            Console.WriteLine("Topic 9 : Methods"); // -> display title
+            // this will show message "Hi"
+            sayHi();
+
+            // this code will do for loop and output has five times of "Hello"
+            for (int i = 1; i < 5; i++)
+            {
+                sayHello();
+            }
+        }
+
+        // this method will say "Hello"
+        static void sayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        // this method will say "Hi"
+        static void sayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
