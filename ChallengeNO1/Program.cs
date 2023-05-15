61963d179890218f69b1b2954d7ab25c9b38413c
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-15 16:25:07 +0800

    This is challenge no1.

---
+++ ChallengeNO1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ChallengeNO1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Challenge #1 */
+            Console.WriteLine("Challenge #1"); // -> display title
+            int[] nums = {1,2,3};
+            Console.WriteLine(summation(nums));
+        }
+
+        static int summation(int[] nums)
+        {
+            int sum = 0;
+            for(int i = 1; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+            return sum;
+        }
+    }
+}
