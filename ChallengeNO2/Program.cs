0d68314981fba196e51fee7a5be8e8e7b872a63a
sairemgloria <sairemgloria1999@gmail.com>
2023-05-16 19:47:28 +0800

    Fix some codes.

--- ChallengeNO2/Program.cs
+++ ChallengeNO2/Program.cs
@@ -15,7 +15,6 @@
             int charPow = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter character voice line : ");
             string charVoiceLine = Console.ReadLine();
-            
 
             CharacterVoiceLine characterVoiceLine = new CharacterVoiceLine(charName, charVoiceLine);
             CharacterClassType characterClassType = new CharacterClassType(charType, charPow);
