8b5e9df0cad94ee8650d1d473a32711a70fed770
sairemgloria <sairemgloria1999@gmail.com>
2023-05-16 13:29:12 +0800

    Added topic NO16_OOP.

---
+++ NO16_OOP/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO16_OOP
+{
+    internal class Person
+    {
+        // attributes
+        public string LastName;
+        public string FirstName;
+        public int Age;
+        public char Gender;
+    }
+}
---
+++ NO16_OOP/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace NO16_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic No 16 : OOP */
+            Console.WriteLine("Topic No 16 : OOP");
+
+            Person personOne = new Person();
+            personOne.FirstName = "Alleria";
+            personOne.LastName = "Galleria";
+            personOne.Age = 20;
+            personOne.Gender = 'F';
+
+            Console.WriteLine(personOne.FirstName + " " + personOne.LastName);
+
+            Person personTwo = new Person();
+            personTwo.FirstName = "Benny";
+            personTwo.LastName = "Saure";
+            personTwo.Age = 22;
+            personTwo.Gender = 'M';
+
+            Person personThree = new Person();
+            personThree.FirstName = "Cally";
+            personThree.LastName = "Fornias";
+            personThree.Age = 21;
+            personThree.Gender = 'F';
+        }
+    }
+}
