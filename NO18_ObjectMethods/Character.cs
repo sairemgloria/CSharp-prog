170585eee51de47760b2d299177c5776b74b0c3c
sairemgloria <sairemgloria1999@gmail.com>
2023-05-17 11:24:26 +0800

    Fix some code.

---
+++ NO18_ObjectMethods/Character.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO18_ObjectMethods
+{
+    internal class Character
+    {
+        public string name, dialog;
+        public int hp, mp, lvl;
+
+        public Character(string name, string dialog, int hp, int mp, int lvl)
+        {
+            this.name = name;
+            this.dialog = dialog;
+            this.hp = hp;
+            this.mp = mp;
+            this.lvl = lvl;
+        }
+
+        public void introduce()
+        {
+            Console.WriteLine("I am " + name);
+        }
+
+        public void sayDialog()
+        {
+            Console.WriteLine(name + "'s Voice Line : " + dialog);
+        }
+
+        public void characterInfo()
+        {
+            Console.WriteLine(name + "'s Information :");
+            Console.WriteLine("Level : " + lvl);
+            Console.WriteLine("HP    : " + hp);
+            Console.WriteLine("MP    : " + mp);
+        }
+    }
+}
---
+++ NO18_ObjectMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NO18_ObjectMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic NO 18 : Object Methods */
+            Console.WriteLine("Topic NO 18 : Object Methods");
+
+            Character character = new Character("Cypher","Greetings!",100,50,1);
+            character.introduce();
+            character.sayDialog();
+            character.characterInfo();
+        }
+    }
+}
