e85c51427e7080e0aebfeb051c98ff74d44f498d
sairemgloria <sairemgloria1999@gmail.com>
2023-05-17 12:13:04 +0800

    Added Challenge NO3.

---
+++ ChallengeNO3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ChallengeNO3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Challenge NO : 3 */
+            Console.WriteLine("Challenge NO    : 3");
+
+            Console.Write("Enter first name    : ");
+            string FirstName = Console.ReadLine();
+
+            Console.Write("Enter last name     : ");
+            string LastName = Console.ReadLine();
+
+            Console.Write("Year level          : ");
+            string Year = Console.ReadLine();
+
+            Console.Write("Course name         : ");
+            string Course = Console.ReadLine();
+
+            Console.Write("Section             : ");
+            string Section = Console.ReadLine();
+
+            Console.Write("Enter prelim grade  : ");
+            float PrelimGrade = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter midterm grade : ");
+            float MidtermGrade = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter final grade   : ");
+            float FinalGrade = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Student studentOne = new Student(FirstName, LastName, Year, Course, Section, PrelimGrade, MidtermGrade, FinalGrade);
+            studentOne.introduceSelf();
+            studentOne.evaluateGrade();
+        }
+    }
+}
---
+++ ChallengeNO3/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeNO3
+{
+    internal class Student
+    {
+
+        /* Attributes of Student */
+        public string firstName, lastName;
+        public string year, course, section;
+        public float prelimGrade, midtermGrade, finalGrade;
+
+        /* Constructor of Student */
+        public Student(string firstName, string lastName, string year, string course, string section, float prelimGrade, float midtermGrade, float finalGrade)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.year = year;
+            this.course = course;
+            this.section = section;
+            this.prelimGrade = prelimGrade;
+            this.midtermGrade = midtermGrade;
+            this.finalGrade = finalGrade;
+        }
+
+        public void introduceSelf()
+        {
+            Console.WriteLine("Student Information");
+            Console.WriteLine();
+            Console.WriteLine("Student Name : " + firstName + " " + lastName);
+            Console.WriteLine("Course       : " + course);
+            Console.WriteLine("Year         : " + year);
+            Console.WriteLine("Section      : " + section);
+            Console.WriteLine();
+        }
+
+        public void evaluateGrade()
+        {
+            float average = (prelimGrade + midtermGrade + finalGrade) / 3;
+            if(average > 100)
+            {
+                Console.WriteLine("Invalid Grade");
+            }
+            else if (average >= 98)
+            {
+                Console.WriteLine("Standing : With Highest Honors");
+            }
+            else if (average >= 95)
+            {
+                Console.WriteLine("Standing : With High Honors");
+            }
+            else if (average >= 90)
+            {
+                Console.WriteLine("Standing : With Honors");
+            }
+            else if (average >= 75)
+            {
+                Console.WriteLine("Standing : Passed");
+            }
+            else
+            {
+                Console.WriteLine("Standing : Failed");
+            }
+            Console.WriteLine("Average : " + average);
+        }
+    }
+}
