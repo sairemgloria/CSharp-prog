5f2283ca11cc04826d29f5223349fddd40bb1338
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-18 12:34:39 +0800

    Added topic NO20_OverloadingConstructor.

---
+++ NO20_OverloadingConstructor/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO20_OverloadingConstructor
+{
+    internal class Product
+    {
+        // Attributes
+        public int productID { get; set; }
+        public string productName { get; set; }
+        public string productDesc { get; set; }
+        public int productStock { get; set; }
+        public float productPrice { get; set; }
+
+        // constructor NO:1
+        public Product(int productID, string productName, string productDesc, int productStock, float productPrice)
+        {
+            this.productID = productID;
+            this.productName = productName;
+            this.productDesc = productDesc;
+            this.productStock = productStock;
+            this.productPrice = productPrice;
+        }
+
+        // constructor NO:2
+        public Product(int productID, string productName, int productStock, float productPrice)
+        {
+            this.productID = productID;
+            this.productName = productName;
+            this.productStock = productStock;
+            this.productPrice = productPrice;
+        }
+    }
+}
---
+++ NO20_OverloadingConstructor/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace NO20_OverloadingConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic NO 20 : Overloading Constructor */
+            Console.WriteLine("Topic NO 20 : Overloading Constructor");
+            Console.WriteLine();
+
+            Product product = new Product(1, "Bearbrand", "Philippines Milk Product", 100, 8.75F);
+            Product productNoDesc = new Product(1, "Alaska", 50, 7.75F);
+
+            Console.WriteLine("Product with Description");
+            Console.WriteLine("Product Serial ID : {0}", product.productID);
+            Console.WriteLine("Name              : {0}", product.productName);
+            Console.WriteLine("Description       : {0}", product.productDesc);
+            Console.WriteLine("Stock             : {0}", product.productStock);
+            Console.WriteLine("Price             : {0}", product.productPrice);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Product w/out Description");
+            Console.WriteLine("Product Serial ID : {0}", productNoDesc.productID);
+            Console.WriteLine("Name              : {0}", productNoDesc.productName);
+            Console.WriteLine("Stock             : {0}", productNoDesc.productStock);
+            Console.WriteLine("Price             : {0}", productNoDesc.productPrice);
+        }
+    }
+}
