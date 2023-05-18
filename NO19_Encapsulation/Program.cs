5afe7d1c14cee43d38454b293d0335f92ecd9e2f
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-18 11:26:44 +0800

    Added topic NO19_Encapsulation.

---
+++ NO19_Encapsulation/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace NO19_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic NO 19 : Encapsulation */
+            Console.WriteLine("Topic NO 19 : Encapsulation");
+
+            // this are the Getters ..
+            try
+            {
+                Console.Write("Enter user id   : "); int UserID = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter username  : "); string Username = Console.ReadLine();
+                Console.Write("Enter firstname : "); string FirstName = Console.ReadLine();
+                Console.Write("Enter lastname  : "); string LastName = Console.ReadLine();
+
+                User u = new User(UserID, Username, FirstName, LastName); // we instantiate the User class ..
+
+                // this are the Setters ..
+                Console.WriteLine("Person Information");
+                Console.WriteLine("UID       : {0}", u.UserID);
+                Console.WriteLine("Username  : {0}", u.Username);
+                Console.WriteLine("Firstname : {0}", u.FirstName);
+                Console.WriteLine("Lastname  : {0}", u.LastName);
+                Console.WriteLine("Fullname  : {0}", u.FullName);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid: Input was not recognized.");
+            }
+        }
+    }
+}
---
+++ NO19_Encapsulation/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO19_Encapsulation
+{
+    internal class User
+    {
+
+        /* Private attributes or fields. */
+        private int userID;
+        private string userName;
+        private string firstName, lastName;
+
+        // Properties of encapsulated attributes.
+        public int UserID
+        {
+            get { return userID; }
+            set { 
+                userID = value;
+                if (value >= 1) userID = value;
+                else Console.WriteLine("Invalid: User ID must not be below 1.");
+            }
+        }
+
+        public string Username
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string FullName
+        {
+            get { return firstName + " " + lastName; }
+        }
+
+        // we can also use constructor, object methods here ...
+        public User(int userID, string userName, string firstName, string lastName)
+        {
+            UserID = userID;
+            Username = userName;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
