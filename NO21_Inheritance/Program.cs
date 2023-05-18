4dbecba19fe4c4effab2f8a2ee13527997506238
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-18 14:32:55 +0800

    Fix some codes.

---
+++ NO21_Inheritance/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace NO21_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic NO 21 : Inheritance */
+            Console.WriteLine("Topic NO 21 : Inheritance");
+
+            /* Person's output */
+            Console.WriteLine("Person Information");
+            Person person = new Person("Brimstone","Male",30);
+            person.introduceSelf(); // this is for person class object method
+            Console.WriteLine();
+
+            /* Toddler's output */
+            Console.WriteLine("Toddler Information");
+            Toddler toddler = new Toddler("Jett","Female",28,"Dash");
+            //toddler.name = "Lyca";
+            //toddler.sex = "Female";
+            //toddler.age = 28;
+            //toddler.Power = "Dash"; // this is the encapsulated attribute of Toddler class
+            toddler.introduceSelf(); // this is for person class object method
+            toddler.toddlerPower(); // this is for toddler class object method
+        }
+    }
+
+    // Person Class
+    class Person
+    {
+        // person attribute(s)
+        public string name { get; set; }
+        public string sex { get; set; }
+        public int age { get; set; }
+
+        // person constructor
+        public Person(string name, string sex, int age)
+        {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
+
+        // person object methods
+        public void introduceSelf()
+        {
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Sex  : " + sex);
+            Console.WriteLine("Age  : " + age);
+        }
+    }
+
+    // Toddler Class : Interited Person
+    class Toddler : Person
+    {
+        // Toddler encapsulated attribute(s)
+        private string power;
+
+        // Toddler getter and setter
+        public string Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+
+        // toddler constructor
+        public Toddler(string name, string sex, int age, string Power) : base(name, sex, age)
+        {
+            this.Power = Power;
+        }
+
+        // Toddler object methods
+        public void toddlerPower()
+        {
+            Console.WriteLine("Ability : " + power);
+        }
+    }
+}
