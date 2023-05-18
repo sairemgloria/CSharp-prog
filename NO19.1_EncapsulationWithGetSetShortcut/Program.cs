9157e6249e5642b290b08867bf88d5bbc3d38b06
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-18 11:45:55 +0800

    Added topic NO19.1_EncapsulationWithGetSetShortcut.

---
+++ NO19.1_EncapsulationWithGetSetShortcut/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace NO19._1_EncapsulationWithGetSetShortcut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Topic NO 19.1 : Encapsulation */
+            Console.WriteLine("Topic NO 19.1 : Encapsulation");
+
+            User user = new User(1000,"HaxHex","Hax","Hex");
+
+            Console.WriteLine("UID       : {0}", user.userID);
+            Console.WriteLine("Username  : {0}", user.userName);
+            Console.WriteLine("Firstname : {0}", user.firstName);
+            Console.WriteLine("lastname  : {0}", user.lastName);
+        }
+    }
+}
---
+++ NO19.1_EncapsulationWithGetSetShortcut/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO19._1_EncapsulationWithGetSetShortcut
+{
+    internal class User
+    {
+
+        // Automatic Properties
+        public int userID
+        {
+            get;
+            set;
+        }
+        public string userName
+        {
+            get;
+            set;
+        }
+        public string firstName
+        {
+            get;
+            set;
+        }
+        public string lastName
+        {
+            get;
+            set;
+        }
+
+        public User(int userID, string userName, string firstName, string lastName)
+        {
+            this.userID = userID;
+            this.userName = userName;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+    }
+}
