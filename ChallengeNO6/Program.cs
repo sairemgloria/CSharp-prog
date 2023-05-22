2dbc15afc800073ad30c5d4527ef138189ac6a71
sairemplacido@gmail.com <sairemplacido@gmail.com>
2023-05-22 11:12:30 +0800

    Added ChallengeNO6.

---
+++ ChallengeNO6/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Collections.Specialized.BitVector32;
+
+namespace ChallengeNO6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Challenge No : 6 */
+            Console.Write("Enter the number of student's to be registered : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Student[] students = new Student[input];
+
+            for (int x = 0; x < students.Length; x++)
+            {
+                Console.WriteLine("Student #" + (x + 1));
+                Console.Write("First Name : ");
+                string FirstName = Console.ReadLine();
+
+                Console.Write("Last Name  : ");
+                string LastName = Console.ReadLine();
+
+                Console.Write("Year       : ");
+                int Year = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Course     : ");
+                string Course = Console.ReadLine();
+
+                Console.Write("Section    : ");
+                char Section = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+
+                students[x] = new Student(FirstName, LastName, Year, Course, Section);
+            }
+
+            foreach (Student student in students)
+            {
+                student.StudentInfo();
+            }
+        }
+    }
+}
---
+++ ChallengeNO6/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeNO6
+{
+    internal class Student
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public int year { get; set; }
+        public string course { get; set; }
+        public char section { get; set; }
+
+        public Student(string firstname, string lastname, int year, string course, char section)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.year = year;
+            this.course = course;
+            this.section = section;
+        }
+
+        public void StudentInfo()
+        {
+            Console.WriteLine("+-+ Student Information +-+");
+            Console.WriteLine();
+            Console.WriteLine("First Name : " + firstname);
+            Console.WriteLine("Last Name  : " + lastname);
+            Console.WriteLine("Year       : " + year);
+            Console.WriteLine("Course     : " + course);
+            Console.WriteLine("Section    : " + section);
+            Console.WriteLine();
+        }
+    }
+}
